f68f301cbc9b0235ab1bc59e3d604e3c65de3df0
E_code <ecode5814@gmail.com>
2023-09-18 08:14:03 +0100

    Completed aritcle uploder programm

--- ConvertMD.py
+++ ConvertMD.py
@@ -1,11 +1,16 @@
 import os, shutil, re
 from markdown import Markdown
-from json import loads, dumps
+import json
 from pathlib import Path
 
 
-md_path = Path(__file__).parent/"Md_Articles"/"Article_description"
 json_path = Path(__file__).parent/"article_path.json"
+
+description_md_path = Path(__file__).parent/"Md_Articles"/"Article_description"
+description_path = Path(__file__).parent/"static"/"docs"/"Article_description_html"
+
+article_md_path = Path(__file__).parent/"Md_Articles"/"Articles" 
+article_path = Path(__file__).parent/"static"/"docs"/"Article_html"
 
 def _get_files(folder_dir):
     files = []
@@ -51,26 +56,93 @@ def _convert_md_to_html(md_files):
     return html_msg
 
 
-#print(_convert_md_to_html(_get_files(md_path)))
 
-def article_description_to_json(json_file):
-    msg_list = _convert_md_to_html(_get_files(md_path))
-    msg_path = _get_files(md_path)
-    html_name = []
+def article_description_to_json(json_file, html_path_list):
+    
+    
+    html_list = []
+    for index, item in enumerate(html_path_list):
 
-    for filepath in msg_path:
+        #Regular experssion pattern to get correct file path
+        pattern = r'(/static/docs/Article_description_html/.*)'
+        match = re.search(pattern, item)
+        short_path = match.group(1)
+
+        html_list.append(short_path)
+
+
+    #adding the file path to json
+    try: #ensure that the json exists and is not empty
+        # Load the existing JSON file
+        with open(json_file, 'r') as json_file_path:
+            json_data = json.load(json_file_path)
+    
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If the file doesn't exist or is empty, initialize json_data as an empty dictionary
+        json_data = {}
+
+    new_list =[]
+    #Adds the paths that are not in json to a new list
+    for item in html_list:
+
+        if item not in json_data.get('files', []):
+            new_list.append(item)
+
+
+    # Add the new content to the top of the existing list
+    json_data['files'] = new_list + json_data.get('files', [])
+
+    # Write the updated JSON data back to the file
+    with open(json_file, 'w') as json_file_path:
+        json.dump(json_data, json_file_path, indent=4)
+
+
+def article_to_html(article_folder, article_storage_folder):
+
+    article_list = _convert_md_to_html(_get_files(article_folder))
+    article_name_path = _get_files(article_folder)
+    article_name = []
+    article_path = []
+
+    #gets markdown file names
+    for filepath in article_name_path:
 
         #get file name
-        name_pattern = rf'{re.escape(str(md_path))}/([^/]+)\.md$'
+        name_pattern = rf'{re.escape(str(article_folder))}/([^/]+)\.md$'
         match = re.search(name_pattern, filepath)
-        
+
         #Add file name to a list
-        html_name.append(str(match.group(1)))
-
-    print(html_name)
+        article_name.append(str(match.group(1)))
 
 
-    #TODO: json file must according to latest modification time
+    #adds article file name to article path
+    for name in article_name:                                                       
+        storage_folder = f"{article_storage_folder}/{name}.html"
+        article_path.append(storage_folder)                               
+    
+    #save markdown to html string to html file
+    num = 0
+    for article in article_list:
 
-article_description_to_json(json_path)
+        #creates and saves the html file
+        with open(article_path[num], 'w') as article_file:
+            article_file.write(article)
+            
+            num += 1
 
+
+    return article_path
+
+article_list = article_to_html(description_md_path, description_path)
+
+article_description_to_json(json_path, article_list)
+if __name__ == "__main__":
+
+    #Convert articles from markdown to  html files
+    article_to_html(article_md_path, article_path)
+
+    #Convert article description snippet from markdown to html
+    description_list = article_to_html(description_md_path, description_path)
+
+    #Add html description path to json file
+    article_description_to_json(json_path, description_list)
---
+++ static/docs/Article_description_html/template_description2.html
@@ -0,0 +1,8 @@
+<div class="row p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+    <div class="col-lg-6 px-0">
+        <h3 class="display-4 fst-italic">Title of a longer featured blog post</h3>
+        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+        <p class="lead mb-0"><a href="/static/docs/articles/template.html" class="text-body-emphasis fw-bold">Continue reading...</a></p>
+        <p class="lead"><i>Uploaded on 15:45 June 2025</i></p>
+    </div>            
+</div>
